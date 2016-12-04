4590834938a6763e62febe5387c39e2143ee673e
都築　航 <twa1224@icloud.com>
2016-12-04 10:58:37 +0000

    Gimfile update for heroku

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,23 +1,45 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
   def index
     @message = Message.new
     @messages =  Message.all
   end
   
+  def edit
+  end
+   
+  def update
+    if @message.update(message_params)
+      redirect_to root_path , notice: "メッセージを編集しました。"
+  else 
+    render 'edit'
+    end
+  end
+  
   def create
     @message = Message.new(message_params)
     if @message.save
-    redirect_to root_path , notice: 'メッセージを保存しました'
-  else 
+   redirect_to root_path , notice: 'メッセージを保存しました'
+    
+   else 
     @messages = Message.all
     flash.now[:alert] = "メッセージの保存に失敗しました。"
     render 'index'
+    end
   end
-end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path , notice: "メッセージを削除しました"
+  end
   
   private 
   def message_params 
     params.require(:message).permit(:name, :body)
   end 
   ##ここまで
+  
+  def set_message
+     @message = Message.find(params[:id])
+  end
 end
